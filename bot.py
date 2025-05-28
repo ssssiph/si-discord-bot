174c88f69c13f41e08f4f597f49220642158b8b9
ssssiph <alt.siph@gmail.com>
2025-05-28 12:51:46 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+from discord.ext import commands
+from db import get_prefix, init_db
+from commands import setup_commands
+from dotenv import load_dotenv
+
+load_dotenv()
+init_db()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+def get_prefix_callable(bot, message):
+    if not message.guild:
+        return "s!"
+    return get_prefix(message.guild.id)
+
+bot = commands.Bot(command_prefix=get_prefix_callable, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Бот запущен как {bot.user}!")
+
+setup_commands(bot)
+
+bot.run(os.getenv("DISCORD_TOKEN"))
