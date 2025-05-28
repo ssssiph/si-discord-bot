dd0a2c23dd4179e2a49ef099bd75490c8df425e5
ssssiph <alt.siph@gmail.com>
2025-05-28 15:18:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ intents.members = True
 
 class MyBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=self.get_prefix, intents=intents)
+        super().__init__(command_prefix=self.get_prefix, intents=intents, help_command=None)
         self.prefixes = {}
         self.db = sqlite3.connect("bot.db")
         self.cursor = self.db.cursor()
