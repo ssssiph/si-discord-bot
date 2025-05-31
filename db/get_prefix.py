8052ce587d4ad09010256ba907e72012ef3824dd
ssssiph <alt.siph@gmail.com>
2025-06-01 02:36:49 +0300

    Update get_prefix.py

--- db/get_prefix.py
+++ db/get_prefix.py
@@ -1,4 +1,4 @@
-from db import execute_query
+from db.db import execute_query
 
 def get_prefix_db(guild_id):
     result = execute_query("SELECT prefix FROM prefixes WHERE guild_id = %s", (guild_id,), fetch_one=True)
