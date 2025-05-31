add78dd122d0f5db0b89d7558c871ee394c53f91
ssssiph <alt.siph@gmail.com>
2025-06-01 02:37:10 +0300

    Update set_prefix.py

--- db/set_prefix.py
+++ db/set_prefix.py
@@ -1,4 +1,4 @@
-from db import execute_query
+from db.db import execute_query
 
 def set_prefix_db(guild_id, prefix):
     execute_query("REPLACE INTO prefixes (guild_id, prefix) VALUES (%s, %s)", (guild_id, prefix))
