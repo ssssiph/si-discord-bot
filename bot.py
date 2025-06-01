7569e3d02777f768eee4d380421a0c97aef717a6
ssssiph <alt.siph@gmail.com>
2025-06-01 03:27:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from db.db import get_prefix
 
 load_dotenv()
 
@@ -13,8 +14,13 @@ intents.members = True
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=self.get_prefix, intents=intents, help_command=None)
-    
+
+    async def get_prefix(self, bot, message):
+        """Получение префикса из базы"""
+        return get_prefix(message.guild.id) if message.guild else "!"
+
     async def setup_hook(self):
+        """Загружаем команды"""
         await self.load_extension("commands.core")
         await self.load_extension("commands.marriage")
         await self.load_extension("commands.verification")
