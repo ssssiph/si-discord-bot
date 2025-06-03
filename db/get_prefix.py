d801bb1ad566c7c69b009a91c9feff2d720b8387
ssssiph <alt.siph@gmail.com>
2025-06-03 03:56:00 +0300

    Delete db/get_prefix.py

--- db/get_prefix.py
+++
@@ -1,5 +0,0 @@
-from db.db import execute_query
-
-def get_prefix_db(guild_id):
-    result = execute_query("SELECT prefix FROM prefixes WHERE guild_id = %s", (guild_id,), fetch_one=True)
-    return result[0] if result else "s!"
