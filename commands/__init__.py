c4647e6833096ca5bb0d3bbce65c86a17fd94036
ssssiph <alt.siph@gmail.com>
2025-06-03 08:07:46 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,12 +1,12 @@
 from .core import setup as core_setup
-from .general import setup as general_setup
+from .marriage import setup as marriage_setup
 
 async def setup(bot):
     try:
         print("Загружаю core...")
         await core_setup(bot)
-        print("Загружаю general...")
-        await general_setup(bot)
+        print("Загружаю marriage...")
+        await marriage_setup(bot)
         print("Все расширения загружены!")
     except Exception as e:
         print(f"Ошибка в setup: {e}")
