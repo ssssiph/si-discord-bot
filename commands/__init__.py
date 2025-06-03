1397440182adc309dda1df314e56707cc74a1a33
ssssiph <alt.siph@gmail.com>
2025-06-03 07:36:00 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,12 +1,9 @@
 from .core import setup as core_setup
-from .general import setup as general_setup
 
-async def setup(bot):
+def setup(bot):
     try:
         print("Загружаю core...")
-        await core_setup(bot)
-        print("Загружаю general...")
-        await general_setup(bot)
+        core_setup(bot)
         print("Все расширения загружены!")
     except Exception as e:
         print(f"Ошибка в setup: {e}")
