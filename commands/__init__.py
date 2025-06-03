9df16ea70e99115b7006a5cc8b4b818fec4a127d
ssssiph <alt.siph@gmail.com>
2025-06-03 06:50:36 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,21 +1,10 @@
-# from .general import setup as setup_general
-# from .core import setup as setup_core
-# from .marriage import setup as setup_marriage
-# from .verification import setup as setup_verification
+from .general import setup as setup_general
+from .core import setup as setup_core
+from .marriage import setup as setup_marriage
+from .verification import setup as setup_verification
 
-# def setup_commands(bot):
-#     setup_general(bot)
-#     setup_core(bot)
-#     setup_marriage(bot)
-#     setup_verification(bot)
-
-from .core import setup as core_setup
-from .general import setup as general_setup
-
-def setup(bot):
-    try:
-        core_setup(bot)
-        general_setup(bot)
-    except Exception as e:
-        print(f"Ошибка в setup: {e}")
-        raise
+def setup_commands(bot):
+    setup_general(bot)
+    setup_core(bot)
+    setup_marriage(bot)
+    setup_verification(bot)
