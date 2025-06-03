fb356eec225ab90ec6b05ca3173af4da6b0aa4a2
ssssiph <alt.siph@gmail.com>
2025-06-03 03:53:56 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,1 +1,10 @@
+from .general import setup as setup_general
+from .core import setup as setup_core
+from .marriage import setup as setup_marriage
+from .verification import setup as setup_verification
 
+def setup_commands(bot):
+    setup_general(bot)
+    setup_core(bot)
+    setup_marriage(bot)
+    setup_verification(bot)
