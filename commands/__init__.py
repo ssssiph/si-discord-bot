aa44cef9f996d1b2ee4cd66d7f9ee4b3255f7b70
ssssiph <alt.siph@gmail.com>
2025-06-03 06:56:57 +0300

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,10 +1,13 @@
-from .general import setup as setup_general
-from .core import setup as setup_core
-from .marriage import setup as setup_marriage
-from .verification import setup as setup_verification
+from .core import setup as core_setup
+from .general import setup as general_setup
 
-def setup_commands(bot):
-    setup_general(bot)
-    setup_core(bot)
-    setup_marriage(bot)
-    setup_verification(bot)
+def setup(bot):
+    try:
+        print("Загружаю core...")
+        core_setup(bot)
+        print("Загружаю general...")
+        general_setup(bot)
+        print("Все расширения загружены!")
+    except Exception as e:
+        print(f"Ошибка в setup: {e}")
+        raise
